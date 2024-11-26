44a791b618cbd475ac07cee89135e73c203b8f92
anntri2001 <ann.trinh@mail.mcgill.ca>
2024-11-26 16:10:08 +0000

    Added scripts

---
+++ meeting-notes/meeting-11-21.md
@@ -0,0 +1,17 @@
+updates:
+
+- exclude documentaries
+- how to collect data:
+    - url: https://api.thenewsapi.com/v1/news/all
+    - search: full title of movies
+        - e.g. "Inside Out 2"
+        - problem: for common words like "Tuesday" how to filter to make sure it is just movies
+    - search_field: default
+    - categories: entertainment
+    - domains: TODO check domains, might be better to exclude
+    - language: en
+    - TODO doing a small range for finding articles
+    - published_after: TODO above
+    - published_before: TODO above
+
+    - TODO zip the typology
--- newscover/json_to_tsv.py
+++ newscover/json_to_tsv.py
@@ -27,5 +27,24 @@ def json_to_tsv():
     tsv_fname = os.path.join(os.path.dirname(__file__), args.output)
     convert(json_fname, tsv_fname)
 
+def convert_to_one_file(json_directory, tsv_fname):
+    combined_data = []
+
+    for filename in os.listdir(json_directory):
+        if filename.endswith('.json'):
+            file_path = os.path.join(json_directory, filename)
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+            #     combined_data.extend(data)
+
+    print(data)
+
+    # with open(tsv_fname, mode='w', newline='') as f:
+    #     writer = csv.DictWriter(f, fieldnames=combined_data[0].keys(), delimiter='\t')
+    #     writer.writeheader()
+    #     writer.writerows(combined_data)
+
 if __name__ == '__main__':
-    json_to_tsv()
+    # json_to_tsv()
+    json_dir_path = "/home/atrinh/final-project/comp370-f24-final-project/newscover/data/jsons"
+    convert_to_one_file(json_dir_path, "all_titles.tsv")
